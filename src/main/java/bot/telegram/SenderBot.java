273e7c5f787fb5cda1c3a3a2abb112f43aa71099
Alexandr Korotkov <element.korotkov@gmail.com>
2022-02-17 18:30:17 +0300

    small refactor

--- src/main/java/bot/telegram/SenderBot.java
+++ src/main/java/bot/telegram/SenderBot.java
@@ -4,10 +4,12 @@ import bot.telegram.repo.TelegramRepo;
 import bot.telegram.repo.TelegramRepoImpl;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,7 +19,6 @@ public class SenderBot extends TelegramLongPollingBot {
     private static List<Long> oldChats = new ArrayList<>();//чаты, подтянутые из БД
     private static Map<Long, String> newChats = new HashMap<>();//чаты, сохраненные с момента последенего запуска программы
     private TelegramRepo telegramRepo = new TelegramRepoImpl();
-
 
     //обновление в бд
     public void synchronizeChats() {
@@ -30,6 +31,7 @@ public class SenderBot extends TelegramLongPollingBot {
         synchronizeChats();
         super.onClosing();
     }
+
     @Override
     public void onRegister() {
         //заполняем список уже существующими ID чатов
@@ -47,19 +49,16 @@ public class SenderBot extends TelegramLongPollingBot {
         return "5121355453:AAHz9S07uwS4D-djpGXdl1B22U9qpOv2lRs";
     }
 
-//    public void sendOurSpecificPDF(InputFile fileToSend) throws TelegramApiException {
-//        execute(new SendDocument().builder().chatId(().toString()).document(fileToSend).build());
-//    }
-
     private void moveNewsToOlds() {
         oldChats.addAll(newChats.keySet());
         newChats.clear();
     }
 
     public void sendAll(InputFile file) {
-        //рассылка для стареньких
+
         for (Long l : oldChats) {
             try {
+                execute(SendPhoto.builder().chatId(l.toString()).photo(new InputFile(new File("resources/111.jpg"))).build());
                 execute(SendMessage.builder().chatId(l.toString()).text(file + " Старенький").build());
             } catch (TelegramApiException e) {
                 e.printStackTrace();
@@ -67,6 +66,7 @@ public class SenderBot extends TelegramLongPollingBot {
         }
         for (Map.Entry<Long, String> pair : newChats.entrySet()) {
             try {
+                execute(SendPhoto.builder().chatId(pair.getKey().toString()).photo(new InputFile(new File("resources/111.jpg"))).build());
                 execute(SendMessage.builder().chatId(pair.getKey().toString()).text(file + " новенький").build());
             } catch (TelegramApiException e) {
                 e.printStackTrace();
@@ -76,7 +76,6 @@ public class SenderBot extends TelegramLongPollingBot {
 
     }
 
-
     @Override
     public void onUpdateReceived(Update update) {
         Long chatId = update.getMessage().getChatId();
@@ -85,24 +84,12 @@ public class SenderBot extends TelegramLongPollingBot {
             String value = update.getMessage().getFrom().getFirstName() + " "
                     + update.getMessage().getFrom().getLastName();
             newChats.put(key, value);
-            System.out.println("Добавили в newChats "+ key + " "+ value);
+            System.out.println("Добавили в newChats " + key + " " + value);
             synchronizeChats();//проверить - будет писать что уже старый
-
         }
         System.out.println(update.getMessage().getFrom().getFirstName() + " "
                 + update.getMessage().getFrom().getLastName() +
                 " chatId = " + update.getMessage().getChat().getId());
-
-//        sendAll("хуй");
-//        chatIdMap.putIfAbsent(update.getChatMember().getChat().getId(),
-//                update.getMessage().getFrom().getFirstName()+" "
-//                        +update.getMessage().getFrom().getLastName()
-//                        +" chatId = "+update.getMessage().getChat().getId());
-//        chatId = update.getMessage().getChatId();
-//        System.out.println(super.);
-//        execute(Se)
-//        sendOurSpecificPDF();
-//            execute(new SendMessage(Long.toString(chatId),"Здарова"));
 
     }
 }
